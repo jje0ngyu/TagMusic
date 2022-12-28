98c261a61d4b1a5c0ef82a92aead9bd79ed43d57
laudable0603 <109578835+laudable0603@users.noreply.github.com>
2022-12-28 17:05:16 +0900

    [ys] sql문수정

---
+++ src/main/java/com/gdu/tagmusic/domain/CouponUseDTO.java
@@ -0,0 +1,20 @@
+package com.gdu.tagmusic.domain;
+
+import java.util.Date;
+
+import groovy.transform.builder.Builder;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class CouponUseDTO {
+	private int couponNo;
+	private String email;
+	private String couponCode;
+	private int couponStatus;
+	private Date couponDate; 
+}
